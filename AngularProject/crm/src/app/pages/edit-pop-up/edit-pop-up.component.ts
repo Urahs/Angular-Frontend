9618f81f78446acfdcd687f88273bbe8ba00c476
b21727685 <b21727685@cs.hacettepe.edu.tr>
2021-07-29 16:34:25 +0300

    ...

--- AngularProject/crm/src/app/pages/edit-pop-up/edit-pop-up.component.ts
+++ AngularProject/crm/src/app/pages/edit-pop-up/edit-pop-up.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-pop-up',
+  selector: 'edit-pop-up',
   templateUrl: './edit-pop-up.component.html',
   styleUrls: ['./edit-pop-up.component.css']
 })
