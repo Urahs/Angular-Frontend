17ea1694dfd705b839590a3e07e1377d48fac2ee
b21727685 <b21727685@cs.hacettepe.edu.tr>
2021-08-02 14:22:53 +0300

    multiple router added

--- AngularProject/crm/src/app/auth/auth.module.ts
+++ AngularProject/crm/src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InitialComponent } from './initial/initial.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     
     SigninComponent,
     SignupComponent,
+    InitialComponent,
   ],
   imports: [
     CommonModule,
