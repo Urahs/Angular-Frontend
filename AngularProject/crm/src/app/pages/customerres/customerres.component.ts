ab956a74cb125a0d83f817d55b5cd4b657a08a51
batuhanozunalan <batuhanozunalan27@gmail.com>
2021-08-02 17:52:11 +0300

    son

--- AngularProject/crm/src/app/pages/customerres/customerres.component.ts
+++ AngularProject/crm/src/app/pages/customerres/customerres.component.ts
@@ -27,6 +27,11 @@ export class CustomerresComponent implements OnInit {
         this.openPreviewModal = messageFromChild;
     }
 
+    
+    postMessageAdd(messageFromChild: any){
+        this.openAddModal = messageFromChild;
+    }
+
     getCustomers(){
         this.postService.get().subscribe(
             (data) => {
